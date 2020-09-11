09a24f17eb84b0729a666403d11f8b0dad29d0c0
Андрей <anastasiiapegar@gmail.com>
2020-09-11 15:32:40 +0300

    Updates

---
+++ precache-manifest.56a4ac23f7dd7206494d311f7ca1e966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc19bdfa5b34ca09098b0c4023b93efe",
+    "url": "/Moskvin_A.V._Portfolio/index.html"
+  },
+  {
+    "revision": "80e75f86f44f7c7387d0",
+    "url": "/Moskvin_A.V._Portfolio/static/css/main.3af82608.chunk.css"
+  },
+  {
+    "revision": "a4f19c7ad31a16b54b72",
+    "url": "/Moskvin_A.V._Portfolio/static/js/2.db5c210e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Moskvin_A.V._Portfolio/static/js/2.db5c210e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e75f86f44f7c7387d0",
+    "url": "/Moskvin_A.V._Portfolio/static/js/main.6cf868f9.chunk.js"
+  },
+  {
+    "revision": "2e8816051bb6979ea3af",
+    "url": "/Moskvin_A.V._Portfolio/static/js/runtime-main.695d08be.js"
+  }
+]);
